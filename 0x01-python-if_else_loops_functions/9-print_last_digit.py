337d01c276c9223af7166a8171bfaef8b7aca21d
Adetunji <adekunleadetunjiwilson@gmail.com>
2023-06-06 07:06:14 -0700

    Solution to task 10

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    if number < 0 and number % 10 != 0:
+        print(10 - number % 10, end="")
+        return (10 - number % 10)
+    else:
+        print(number % 10, end="")
+        return (number % 10)
