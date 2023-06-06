db0ad1c11cb470cc8dd112a4f929f8155c8e60c5
Adetunji <adekunleadetunjiwilson@gmail.com>
2023-06-06 05:24:07 -0700

    Removed type casting

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last = int(str(number)[-1])
-if number < 0:
-    last *= -1
+last = number % 10
 if last > 5:
-    print(f"Last digit of {number:d} is {last:d} and is greater than 5")
+    print(f"Last digit of {number} is {last:d} and is greater than 5")
 elif last == 0:
     print(f"Last digit of {number:d} is {last:d} and is 0")
 elif (last < 6) and (last != 0):
