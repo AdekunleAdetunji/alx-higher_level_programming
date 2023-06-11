459214159bcf592c4d7fe4bd81b46e4b157aab1c
Adetunji <adekunleadetunjiwilson@gmail.com>
2023-06-10 23:52:42 -0700

    Assigned a value to my_string variable

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    if not my_string:
-        return
     ls = []
     for char in my_string:
         if char != 'c' or char != "C":
             ls.append(char)
-    return ("".join(ls))
+    my_string = "".join(ls)
+    return (my_string)
