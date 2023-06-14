0c2a4c8f2e23df59c1406e205d92176d6f6508d3
Adetunji <adekunleadetunjiwilson@gmail.com>
2023-06-14 05:53:05 -0700

    Changed return value

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     if not my_list:
-        return
+        return []
     else:
         return [x if x != search else replace for x in my_list]
