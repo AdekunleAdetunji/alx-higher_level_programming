9bb566b466728140c58b4bd25cc30e4e0bce3875
Adetunji <adekunleadetunjiwilson@gmail.com>
2023-06-26 04:56:59 -0700

    corrected variable declaration error

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,11 +2,11 @@
 
 def safe_print_list(my_list=[], x=0):
     try:
-        index = 0
+        idx = 0
         for index in range(x):
             print("{}".format(my_list[index]), end="")
-        print("")
-        return (index + 1)
+            idx += 1
     except Exception as e:
-        print("")
-        return (index)
+        pass
+    print("")
+    return idx
