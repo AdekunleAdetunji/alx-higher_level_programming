e339e37e7cfaffd3dd78696c2d036fcf0a38c745
Adetunji <adekunleadetunjiwilson@gmail.com>
2023-06-27 06:31:55 -0700

    solution to 3-task

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""This module contains a new version of the square
+class defined in 2-task"""
+
+
+class Square():
+    """This is a richer version of square class in 2-task with
+    an additional method"""
+    def __init__(self, size=0):
+        """
+        Initialization of the square class
+
+        Args:
+        size: A private instance attribute of the square size
+        """
+        self.__size = size
+        if not isinstance(self.__size, int):
+            raise TypeError("size must be an integer")
+        elif self.__size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """Computes and return the area of the square"""
+        return (self.__size)**2
