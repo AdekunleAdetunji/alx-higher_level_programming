80f9c8967a1784f38712dafc5a92e7b7058d1e01
Adetunji <adekunleadetunjiwilson@gmail.com>
2023-06-27 05:45:57 -0700

    Added documentation for module and class

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""File demonstration how to define an empty python class"""
+
 
 class Square():
+    """An empty class that defines a square"""
     pass
