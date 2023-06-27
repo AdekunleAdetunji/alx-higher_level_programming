b46f304bb13613b00cf1f28f8233abd967a6b09c
Adetunji <adekunleadetunjiwilson@gmail.com>
2023-06-27 06:03:04 -0700

    solution to 1-task

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""This script contains a class defined by the following
+Private instance attribute: size
+Instantiation with size (no type/value verification)
+You are not allowed to import any module
+"""
+
+
+class Square():
+    """This class defines a square"""
+    def __init__(self, size):
+        """
+        Initialization of the square class
+
+
+        Args:
+        size: The size of the square
+        """
+        self.__size = size
