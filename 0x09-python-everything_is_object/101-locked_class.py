da9d423eecc75929e9f06454e45c3525464d191b
Adetunji <adekunleadetunjiwilson@gmail.com>
2023-07-04 06:10:47 -0700

    Added documentation for the class attribute and a space between docuementation and code

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
 """
-This module contains a locked class that prevents the addition of a new isntance
-attribute except the attribute is first_name
+This module contains a locked class that prevents the addition of a new
+instance attribute except the attribute is first_name
 """
+
+
 class LockedClass():
     """
     This class creates a new  instance that does not allow the allocation of a
-
     new instance attirbute without it beign first_name
+    @attrbs:
+    __slots__ = a class attribute of type list with the names of allowed
+    attributes
     """
+
     __slots__ = ["first_name"]
