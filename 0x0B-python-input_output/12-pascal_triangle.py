db00e36951a2561979d214a528990064aeaa1155
adetunji <adekunleadetunjiwilson@gmail.com>
2023-07-11 14:09:40 -0500

    solution to 12-task

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+This module contains a python function that creates a pascal triangle
+"""
+
+
+def find_fac(n):
+    """find the factorial of a value n"""
+    if n == 1 or n == 0:
+        return 1
+    return n * find_fac(n - 1)
+
+
+def pascal_triangle(n):
+    """
+    creates a pascals triangle of size n
+
+    args
+       @n: The size of the pascal's triangle
+    """
+    result = []
+    if n <= 0:
+        return result
+    for row in range(0, n):
+        row_vals = []
+        for elem in range(0, row + 1):
+            val = int(find_fac(row) / (find_fac(row-elem) * find_fac(elem)))
+            row_vals.append(val)
+        result.append(row_vals)
+    return result
