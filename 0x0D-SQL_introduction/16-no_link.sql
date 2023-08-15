67102ec85dcf96a772c21dd4d1546a1ff0b0d473
adetunji <adekunleadetunjiwilson@gmail.com>
2023-08-15 22:26:07 +0100

    added order mode to task 16

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -6,4 +6,5 @@
 --    The database name will be passed as an argument to the mysql command
 SELECT score, name
 FROM second_table
-WHERE TRIM(name) > ''
+WHERE name IS NOT NULL
+ORDER BY score DESC;
