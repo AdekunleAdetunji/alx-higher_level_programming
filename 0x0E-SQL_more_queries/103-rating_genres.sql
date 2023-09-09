569292c9555f326f4306040008a03be4f911d2fc
adetunji <adekunleadetunjiwilson@gmail.com>
2023-09-09 14:17:17 +0100

    changed the case of an sql keyword

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -4,7 +4,7 @@
 --    Results must be sorted in descending order by the rating
 --    You can use only one SELECT statement
 --    The database name will be passed as an argument of the mysql command
-SELECT tv_genres.name, sum(tv_show_ratings.rate) as rating
+SELECT tv_genres.name, sum(tv_show_ratings.rate) AS rating
 FROM tv_genres
 INNER JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
 INNER JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_show_genres.show_id
