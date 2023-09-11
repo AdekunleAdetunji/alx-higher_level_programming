66f63fb292acd7f26f2c77ca1926d2999e8d43e0
adetunji <adekunleadetunjiwilson@gmail.com>
2023-09-11 16:39:25 +0100

    solution to task-11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,15 +2,17 @@
 
 const argv = process.argv;
 const count = argv.length;
-if (count === 2 || count === 3) {
+if (count <= 3) {
   console.log(0);
 } else {
   let max = Number(argv[2]);
   let max1 = Number(argv[2]);
-  for (let i = 0; i < count; i++) {
+  for (let i = 2; i < count; i++) {
     if (Number(argv[i]) > max) {
       max1 = max;
       max = Number(argv[i]);
+    } else if (Number(argv[i]) > max1) {
+      max1 = Number(argv[i]);
     }
   }
   console.log(max1);
