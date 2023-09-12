48987997cdf85d8c665b8966cdfcb99a28840d5c
adetunji <adekunleadetunjiwilson@gmail.com>
2023-09-12 13:03:07 +0100

    corrected semistandard error

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -5,11 +5,11 @@ This module contains a rectangle class that defines a class with the
 constructor taking two arguments (w, h)
 */
 const Rectangle = class Rectangle {
-    constructor(w, h) {
-	if (w > 0 && h > 0) {
-	    this.width = w;
-	    this.height = h;
-	};
-    };
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
 };
-module.exports = Rectangle
+module.exports = Rectangle;
