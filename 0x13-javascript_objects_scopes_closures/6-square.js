5e6b17de8ef059925a79546e334572e79e3612a2
adetunji <adekunleadetunjiwilson@gmail.com>
2023-09-12 13:07:40 +0100

    referenced the code for task-5

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -4,7 +4,7 @@
 This module contains a rectangle class that defines a class with the
 constructor taking two arguments (w, h)
 */
-const Rectangle = require('./4-rectangle');
+const Rectangle = require('./5-square');
 const Square = class Square extends Rectangle {
   constructor (size) {
     super(size, size);
@@ -13,7 +13,7 @@ const Square = class Square extends Rectangle {
   charPrint (c) {
     if (c) {
       for (let i = 0; i < this.height; i++) {
-        console.log(String(c).repeat(this.height));
+        console.log(String(c).repeat(this.width));
       }
     } else {
       this.print();
