e8c0e271792f8851f4fcb670630aad4ed982dd80
adetunji <adekunleadetunjiwilson@gmail.com>
2023-09-16 14:24:00 +0100

    solution to advanced tasks

---
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""
+This module is a python file that contains class definition for city
+defined in a manner similar to how the state class is defined in state.py
+"""
+
+from relationship_state import Base
+from sqlalchemy import Column, Integer, String, ForeignKey
+
+
+class City(Base):
+    """
+    This class defines the table structure for a city in mysql database
+    """
+    __tablename__ = "cities"
+    id = Column(Integer, primary_key=True, nullable=False,
+                autoincrement=True, unique=True)
+    name = Column(String(128))
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
